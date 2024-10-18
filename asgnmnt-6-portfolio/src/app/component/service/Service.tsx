fd24bb35f9897f5767765e712247dc664c185450
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-10-18 22:10:15 +0500

    Add files via upload

---
+++ asgnmnt-6-portfolio/src/app/component/service/Service.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+import "./Service.css";
+
+export default function Service() {
+  return (
+    <div className="service-page">
+
+      <h1>Service Page</h1>
+      <p>WelCome To Our Services, Bhai Koi Bhi Service Ni Hai Filhal Job Deni Hai To Thik Nai To L**d Pe Charho</p>
+      
+    </div>
+
+    
+  );
+}
